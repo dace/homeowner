71b9a515ee16dd1cc0031f9a97d80a9f0de42657
hidace <dacewillmott@gmail.com>
2016-01-01 18:30:19 -0500

    add ItemsViewController, delete ViewController

---
+++ ItemsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ItemsViewController.swift
+//  Homeowner
+//
+//  Created by Dace Willmott on 1/1/16.
+//  Copyright © 2016 Dace Willmott. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+}
